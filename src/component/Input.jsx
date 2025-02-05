6886b948f2abe3d6f9211e229d58653664f08d2a
mickael-57 <wolfire666@live.fr>
2025-02-05 16:51:58 +0100

    up 2

--- src/component/Input.jsx
+++ src/component/Input.jsx
@@ -3,15 +3,9 @@ import useCalculateContext from "../context/CalculateContext.jsx";
 const Input = ({label, name, value}) => {
  
  const {state, dispatch} = useCalculateContext()
-//  const value = state[name]
  
  const handleClick = (e) => {
-//   const {value} = e.target
-//   const val = e.target.attributes.datavalue.nodeValue;
-  
-//   if (!/^\d+$/.test(val) && val.trim() !== "") {
-//    return
-//   }
+
   console.log(value)
   dispatch({
    type: "set_value",
--- src/reducer/calculateReducer.jsx
+++ src/reducer/calculateReducer.jsx
@@ -1,50 +1,5 @@
 import {useReducer} from "react";
 
-// const initialState = {
-//  a: "",
-//  b: "",
-//  result: "",
-//  error: ""
-// }
-
-// const checkValues = (state) => {
-//  if (state.a === "") return false
-//  if (state.b === "") return false
-//  return true
-// }
-
-// const reducer = (state,action) => {
-//  switch(action.type) {
-  
-//   case "set_value":
-//    return {
-//     ...state,
-//     [action.payload.name]: action.payload.value,
-//     error: ""
-//    }
-   
-//   case "calculate":
-//    return checkValues(state) ? {
-//     ...state,
-//     a: "",
-//     b: "",
-//     error: "",
-//     result: eval(`${state.a}${action.payload}${state.b}`)
-//    } : {
-//     ...state,
-//     error: "Merci de remplir tous les champs avant d'effectuer un calcul"
-//    }
-   
-//   case "reset":
-//    return initialState;
-  
-  
-//   default:
-//    return state;
-//  }
-// }
-
-/* -------------------------------------------------------------------------- */
 const initialState = {
   result: "",
   prevValue: null,
@@ -97,8 +52,6 @@ const reducer = (state, action) => {
       return state;
   }
 }
-
-/* -------------------------------------------------------------------------- */
 
 const useCalculateReducer = () => useReducer(reducer, initialState)
 
